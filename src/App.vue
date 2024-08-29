b20aea0d0b729e7ceb4e48cc998dc1e455a31bd5
mac1 <mac1@MacBookPro-2.local>
2024-08-29 22:31:35 +0500

    added new functions

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
 <h2>Hello World !</h2>
+<input type="text" placeholder="ismingizni kiriting" v-model="name">
   </div>
 </template>
 
 <script setup>
-import { watch, ref} from 'vue';
-const name= ref("");
+import { watch, watchEffect, ref} from 'vue';
+const name= ref("Jamila");
 
 watch(name, (newValue, oldValue)=>{
   console.log(`ism o'zgardi: ${newValue} dan ${oldValue}` );
+})
+
+watchEffect(()=>{
+  console.log("effect:", name.value);
   
 })
+console.log("no effect:", name.value);
 
 </script>
 
