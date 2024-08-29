b3d5caae58a05d7b584b39bdd42d03c32b292334
mac1 <mac1@MacBookPro-2.local>
2024-08-29 22:21:50 +0500

    commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+<h2>Hello World !</h2>
+  </div>
+</template>
+
+<script setup>
+import { watch, ref} from 'vue';
+const name= ref("");
+
+watch(name, (newValue, oldValue)=>{
+  console.log(`ism o'zgardi: ${newValue} dan ${oldValue}` );
+  
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
